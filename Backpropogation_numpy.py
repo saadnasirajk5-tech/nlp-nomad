2393f0c8e8db0a540f3e0ae67f18d68e5e95bdbe
saadnasirajk5-tech <saadnasirajk5@gmail.com>
2025-11-20 09:45:40 -0800

    First one

--- Backpropogation_numpy.py
+++ Backpropogation_numpy.py
@@ -1,7 +1,15 @@
 import numpy as np  
 # It's the XOR problem  
-# DataSetup
-
+# DataSetup 
+"""X is input data and y is true labels"""
+X= np.array([[0,0], 
+             [0,1],
+             [1,0],
+             [1,1]])  
+y = np.array([[0],[1],[1],[0]])
+np.random.seed(42) # For reproducibility 
+# Architecture is 2 input neurons 4 hidden neurons and 1 output neuron 
+input_size = 2 
 
 
 
